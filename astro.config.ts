b793570a2a08306d87473e212ab53e3217f38393
alexzhang1030 <1642114555@qq.com>
2022-10-13 01:04:35 +0800

    fix: build lost island style

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
-import Unocss from 'unocss/astro';
+import { defineConfig } from 'astro/config'
+import Unocss from 'unocss/astro'
 
 // https://astro.build/config
-import solidJs from "@astrojs/solid-js";
+import solidJs from '@astrojs/solid-js'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [Unocss(), solidJs()]
-});
+  integrations: [Unocss(), solidJs()],
+})
--- src/components/HeaderIcons.tsx
+++ src/components/HeaderIcons.tsx
@@ -1,10 +1,7 @@
 export function Github() {
   return (
     <div
-      i-line-md:github-loop
-      w-30px
-      h-30px
-      cursor-pointer
+      class="i-line-md:github-loop w-30px h-30px cursor-pointer"
       onClick={() => window.open('https://github.com/alexzhang1030/views')}
     >
     </div>
@@ -13,8 +10,9 @@ export function Github() {
 
 export function AstroIcon() {
   return (
-      <span cursor-pointer
-          bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent
+      <span
+      class="cursor-pointer
+          bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent"
           onClick={() => window.open('https://astro.build/')}>
       Astro
     </span>
