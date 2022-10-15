af0b2c5801ce3d9d98d334e4a6954233acb2b285
alexzhang1030 <1642114555@qq.com>
2022-10-16 01:51:09 +0800

    chore: remove vercel config

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from 'astro/config'
 import Unocss from 'unocss/astro'
 
-// https://astro.build/config
 import solidJs from '@astrojs/solid-js'
 
-// https://astro.build/config
-import vercel from '@astrojs/vercel/serverless'
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [Unocss(), solidJs()],
-  output: 'server',
-  adapter: vercel(),
 })
