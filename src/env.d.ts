90c3fd86f04d59d14cda2668258cb23873533ba7
alexzhang1030 <1642114555@qq.com>
2022-10-17 11:18:48 +0800

    refactor: refactor Layout

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="astro/client" />
+
+import type { AttributifyAttributes } from 'unocss/preset-attributify'
+
+declare global {
+  namespace astroHTML.JSX {
+    interface HTMLAttributes extends AttributifyAttributes { }
+  }
+}
+
+declare module 'solid-js' {
+  namespace JSX {
+    interface HTMLAttributes<T> extends AttributifyAttributes {}
+  }
+}
---
+++ src/pages/Button/_Button.tsx
@@ -0,0 +1,8 @@
+export const Button = () => {
+  return () => (
+    <button class="
+      px-3 py-1.5 bg-#E0EAFB color-#2080f0 rounded-1 transition-colors
+      hover:bg-#D5E3FA
+    ">Button</button>
+  )
+}
