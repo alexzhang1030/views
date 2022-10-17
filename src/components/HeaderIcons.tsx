2737f73cd2b8e9bc98160f1acd37febd559d34e9
alexzhang1030 <1642114555@qq.com>
2022-10-17 17:45:39 +0800

    feat: add composable and toggle dark

--- src/components/HeaderIcons.tsx
+++ src/components/HeaderIcons.tsx
@@ -1,3 +1,18 @@
+import { useDark } from '@composables/useDark'
+
+export function ToggleDark() {
+  const [dark, setDark] = useDark()
+
+  const toggleDark = () => {
+    setDark(!dark())
+  }
+  return (
+    <button class="icon-btn mx-2 !outline-none" onclick={() => toggleDark()}>
+      <div class="i-carbon-sun dark:i-carbon-moon" />
+    </button>
+  )
+}
+
 export function Github() {
   return (
     <div
