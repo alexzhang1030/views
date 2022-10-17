ccc516bbde49cb449c0c1a5e2de226b0ee213fcd
alexzhang1030 <1642114555@qq.com>
2022-10-17 12:23:47 +0800

    feat: add button with wave

--- src/pages/Button/_Button.tsx
+++ src/pages/Button/_Button.tsx
@@ -1,8 +1,21 @@
+import './_Button.css'
+
 export const Button = () => {
+  let wave: HTMLDivElement
+  const handleWaveEnd = () => {
+    wave.classList.remove('button')
+  }
+  const handleClick = () => {
+    handleWaveEnd()
+    wave.classList.add('button')
+  }
   return () => (
     <button class="
-      px-3 py-1.5 bg-#E0EAFB color-#2080f0 rounded-1 transition-colors
-      hover:bg-#D5E3FA
-    ">Button</button>
+      px-3 py-1.5 bg-#E0EAFB color-#2080f0 rounded-1 transition-colors relative
+      hover:bg-#D5E3FA active:bg-#CADBF9 focus-visible:bg-#D5E3FA focus-visible:outline-none
+    " onClick={handleClick}>
+      <span>Button With Wave</span>
+      <div ref={wave} class="pointer-events-none absolute top-0 left-0 w-full h-full rounded-1" onAnimationEnd={handleWaveEnd}></div>
+    </button>
   )
 }
